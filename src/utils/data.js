774dbefb5986b9d52c5ea7bfabce2b322a241d23
ahtasham <ahtaxam03@gmail.com>
2024-05-25 16:45:45 +0500

    utils folder

---
+++ src/utils/data.js
@@ -0,0 +1,49 @@
+import PunjabMap from "../assets/images/punjabmap.png";
+import SindhMap from "../assets/images/sindhmap.jpg";
+import KpkMap from "../assets/images/kpkmap.jpg";
+
+export const SOLUTIONS = [
+  {
+    image:
+      "https://assets-global.website-files.com/61c6baed8b3d470519824903/61cbeeecf16cb010ceb4ced3_Grouploadify-services9704.svg",
+    heading: "Dry Run",
+    text: `Dry van transportation offers a versatile solution for transporting various types of dry goods, ranging from furniture and retail merchandise to non-perishable items. 
+      This shipping method utilizes enclosed trailers.`,
+  },
+  {
+    image:
+      "https://assets-global.website-files.com/61c6baed8b3d470519824903/61cbeeec3647f8e929ecfe4c_Grouploadify-services9705.svg",
+    heading: "FLATBED & SPECIALIZED ",
+    text: `Loadify plays a crucial role in serving industries such as Construction, Lumber, and Steel by offering specialized equipment movement, expert knowledge,
+       and a vast network. These industries rely on Loadify's.`,
+  },
+  {
+    image:
+      "https://assets-global.website-files.com/61c6baed8b3d470519824903/6234d9aebd819f0f1bd1f410_324.svg",
+    heading: "DROP TRAILERS",
+    text: `Our trailer pool includes a Drop & Hook program designed specifically for high-volume shipping customers operating in fast-paced industries. 
+      This program allows businesses to efficiently manage their logistics.`,
+  },
+  {
+    image:
+      "https://assets-global.website-files.com/61c6baed8b3d470519824903/61cbeeec2dcd0d6179583190_Grouploadify-services9709.svg",
+    heading: "TEMPERATURE CONTROLLED",
+    text: `Our refrigerated carrier partners are equipped with state-of-the-art cloud technology, enabling them to provide advanced in-transit temperature monitoring capabilities.
+       This ensures that perishable goods are transported under optimal conditions.`,
+  },
+];
+
+export const DEVILERYPOINT = [
+  {
+    image: PunjabMap,
+    text: "Punjab, Pakistan",
+  },
+  {
+    image: KpkMap,
+    text: "Kpk, Pakistan",
+  },
+  {
+    image: SindhMap,
+    text: "Sindh, Pakistan",
+  },
+];
