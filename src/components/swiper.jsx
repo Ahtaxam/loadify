3c61308b34761381ff1d91ddf12d016462e3ac86
Ahtaxam <ahtaxam03@gmail.com>
2024-05-27 15:57:18 +0500

    fix swipper responsiveness issue

--- src/components/swiper.jsx
+++ src/components/swiper.jsx
@@ -1,29 +1,43 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 
-import "swiper/css";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-import TruckAddCard from "./truckAddCard";
-import InventoryAddCard from "./inventoryAddCard";
+import TruckAddCard from './truckAddCard';
+import InventoryAddCard from './inventoryAddCard';
 
 function SwiperComponent({ data, text, type }) {
   return (
     <>
-      <p className="text-2xl pl-8">{text}</p>
-      <div className=" p-8">
+      <p className='text-2xl pl-8'>{text}</p>
+      <div className=' p-8'>
         <Swiper
           modules={[Navigation]}
           spaceBetween={10}
-          slidesPerView={5}
           navigation={true}
-          // onSlideChange={() => console.log("slide change")}
-          // onSwiper={(swiper) => console.log(swiper)}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            480: {
+              slidesPerView: 2,
+            },
+            640: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+          }}
         >
           {data.map((data, index) => (
             <SwiperSlide key={index}>
-              {type === "truck" ? (
+              {type === 'truck' ? (
                 <TruckAddCard data={data} />
               ) : (
                 <InventoryAddCard data={data} />
