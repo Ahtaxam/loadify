57bd18d0f73e7b357de78eb06408b69d29ca7a88
Ahtaxam <ahtaxam03@gmail.com>
2024-05-30 17:59:37 +0500

    remove protected route

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { PATH } from "../utils/path";
-import Home from "../screens//landing";
-import Login from "../screens/auth/Login";
-import Signup from "../screens/auth/signup";
-import HomeMain from "../screens/home";
-import ProtectedRoute from "./ProtectedRoute";
-import TruckLoader from "../screens/loader";
-import InventoryDetail from "../components/inventoryDetail";
-import RoleRoute from "./RoleRoute";
-import InventoryHolder from "../screens/inventory";
-import LoaderDetail from "../components/loaderDetail";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { PATH } from '../utils/path';
+import Home from '../screens//landing';
+import Login from '../screens/auth/Login';
+import Signup from '../screens/auth/signup';
+import HomeMain from '../screens/home';
+import ProtectedRoute from './ProtectedRoute';
+import TruckLoader from '../screens/loader';
+import InventoryDetail from '../components/inventoryDetail';
+import RoleRoute from './RoleRoute';
+import InventoryHolder from '../screens/inventory';
+import LoaderDetail from '../components/loaderDetail';
 import UnAuthorized from '../screens/unAuthorized';
 
 function AppRoute() {
@@ -21,54 +21,22 @@ function AppRoute() {
         <Route path={PATH.LOGIN} element={<Login />} />
         <Route path={PATH.SIGNUP} element={<Signup />} />
 
-        <Route path={PATH.USERSHOME} element={<ProtectedRoute>
-        <HomeMain/>
-        </ProtectedRoute>} />
         <Route
-          path={PATH.LOADERADDS}
+          path={PATH.USERSHOME}
           element={
             <ProtectedRoute>
-              <RoleRoute roles={["Truck Loader"]}>
-                {" "}
-                <TruckLoader />
-              </RoleRoute>
+              <HomeMain />
             </ProtectedRoute>
           }
         />
+        <Route path={PATH.LOADERADDS} element={<TruckLoader />} />
 
-        <Route
-          path={PATH.INVENTORYDETAIL}
-          element={
-            <ProtectedRoute>
-              <RoleRoute roles={["Truck Loader"]}>
-                <InventoryDetail />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={PATH.INVENTORYADD}
-          element={
-            <ProtectedRoute>
-              <RoleRoute roles={["Inventory"]}>
-                <InventoryHolder />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
+        <Route path={PATH.INVENTORYDETAIL} element={<InventoryDetail />} />
+        <Route path={PATH.INVENTORYADD} element={<InventoryHolder />} />
 
-        <Route
-          path={PATH.LOADERDETAIL}
-          element={
-            <ProtectedRoute>
-              <RoleRoute roles={["Inventory"]}>
-                <LoaderDetail />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
+        <Route path={PATH.LOADERDETAIL} element={<LoaderDetail />} />
 
-        <Route path={PATH.UNAUTHORIZED} element={<UnAuthorized/>} />
+        <Route path={PATH.UNAUTHORIZED} element={<UnAuthorized />} />
       </Routes>
     </div>
   );
