63f3e7866391130fff21ffc443f8f4aa420c5992
ahtasham <ahtaxam03@gmail.com>
2024-06-04 22:03:57 +0500

    implement delete add func

--- src/components/inventoryDetail.jsx
+++ src/components/inventoryDetail.jsx
@@ -1,80 +1,114 @@
-import React from 'react';
-import ImageCarousel from './imageCarousel';
-import Button from './button';
-import { useParams } from 'react-router-dom';
-import { useGetSingleInventoryQuery } from '../redux/api/inventoryAdd';
-import SpinnerComponent from './spinner';
-import { getCurrentUser, getUserRole } from '../utils/currentUser';
-import NavbarComponent from './navbar';
+import React from "react";
+import ImageCarousel from "./imageCarousel";
+import Button from "./button";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  inventoryAddApi,
+  useDeleteInventoryMutation,
+  useGetSingleInventoryQuery,
+} from "../redux/api/inventoryAdd";
+import SpinnerComponent from "./spinner";
+import { getCurrentUser, getUserRole } from "../utils/currentUser";
+import NavbarComponent from "./navbar";
+import { toast } from "react-toastify";
+import { PATH } from "../utils/path";
+import { useDispatch } from "react-redux";
 
 function InventoryDetail() {
   const { id } = useParams();
   const user = getCurrentUser();
   const role = getUserRole();
   const { data, isLoading } = useGetSingleInventoryQuery(id);
+  const [deleteInventory, { isLoading: loading }] =
+    useDeleteInventoryMutation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const {
-    inventoryType = '',
-    inventorySize = '',
+    postedBy = "",
+    _id = "",
+    inventoryType = "",
+    inventorySize = "",
     inventoryWeight = 0,
-    ownerName = '',
-    phoneNumber = '',
-    location = '',
-    countryName = '',
-    stateName = '',
-    city = '',
+    ownerName = "",
+    phoneNumber = "",
+    location = "",
+    countryName = "",
+    stateName = "",
+    city = "",
     inventoryPicture = [],
   } = data?.data || {};
 
+  const handleDeleteInventory = async () => {
+    try {
+      const { message } = await deleteInventory(id).unwrap();
+      toast.success(message);
+      navigate(PATH.MYADDS);
+      dispatch(inventoryAddApi.util.invalidateTags(["Inventory"]));
+    } catch (error) {
+      console.log(error);
+      toast.error("SERVER ERROR");
+    }
+  };
+
   return (
     <>
-    <NavbarComponent/>
+      <NavbarComponent />
       {isLoading ? (
         <SpinnerComponent />
       ) : (
-        <div className='shadow-xl w-[80%] mx-auto p-4 mt-8'>
-          <p className='text-center text-xl font-bold'>Inventory Detail</p>
-          <div className='flex justify-end'>
-            <Button className='bg-navy w-[100px] hover:bg-white hover:text-black hover:border-2 hover:border-dotted'>
-              Chat
-            </Button>
+        <div className="shadow-xl w-[80%] mx-auto p-4 mt-8">
+          <p className="text-center text-xl font-bold">Inventory Detail</p>
+          <div className="flex justify-end">
+            {postedBy !== user._id ? (
+              <Button className="bg-navy w-[100px] hover:bg-[hsl(0,100%,4%)] hover:text-white ">
+                Chat
+              </Button>
+            ) : (
+              <Button
+                className="bg-red-500 w-[100px] "
+                onClick={handleDeleteInventory}
+              >
+                {loading ? "Deleting..." : "Delete"}
+              </Button>
+            )}
           </div>
-          <div className='flex justify-between flex-wrap p-4'>
+          <div className="flex justify-between flex-wrap p-4">
             <p>
-              <span className='font-bold'>Type: </span> {inventoryType}
+              <span className="font-bold">Type: </span> {inventoryType}
             </p>
             <p>
-              <span className='font-bold'>Size: </span> {inventorySize}
+              <span className="font-bold">Size: </span> {inventorySize}
             </p>
             <p>
-              <span className='font-bold'>Weight: </span> {inventoryWeight}
+              <span className="font-bold">Weight: </span> {inventoryWeight}
             </p>
           </div>
 
-          <div className='flex justify-between flex-wrap p-4'>
+          <div className="flex justify-between flex-wrap p-4">
             <p>
-              <span className='font-bold'>Owner Name: </span> {ownerName}
+              <span className="font-bold">Owner Name: </span> {ownerName}
             </p>
             <p>
-              <span className='font-bold'>Phone Number: </span> {phoneNumber}
+              <span className="font-bold">Phone Number: </span> {phoneNumber}
             </p>
           </div>
-          <p className='p-4'>
-            <span className='font-bold'>Location: </span>
+          <p className="p-4">
+            <span className="font-bold">Location: </span>
             {location}
           </p>
 
-          <div className='flex justify-between flex-wrap p-4'>
+          <div className="flex justify-between flex-wrap p-4">
             <p>
-              <span className='font-bold'>Country: </span> {countryName}
+              <span className="font-bold">Country: </span> {countryName}
             </p>
             <p>
-              <span className='font-bold'>State: </span> {stateName}
+              <span className="font-bold">State: </span> {stateName}
             </p>
             <p>
-              <span className='font-bold'>City: </span> {city}
+              <span className="font-bold">City: </span> {city}
             </p>
           </div>
-          <p className='p-4 font-bold'>Inventory Images</p>
+          <p className="p-4 font-bold">Inventory Images</p>
           <ImageCarousel data={inventoryPicture} />
         </div>
       )}
