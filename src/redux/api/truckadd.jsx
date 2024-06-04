6488dc8e2e2d182017a3beddddb344e1c1214df7
ahtasham <ahtaxam03@gmail.com>
2024-06-04 22:04:59 +0500

    truck add delete api

--- src/redux/api/truckadd.jsx
+++ src/redux/api/truckadd.jsx
@@ -1,16 +1,18 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getToken } from '../../utils/currentUser';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getToken } from "../../utils/currentUser";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const token = getToken();
 
 export const truckAddApi = createApi({
-  reducerPath: 'truckAddApi',
+  reducerPath: "truckAddApi",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
+  tagTypes: ["Inventory"],
   endpoints: (builder) => ({
     getAllLoaders: builder.query({
-      query: () => '/loader/all',
+      query: () => "/loader/all",
+      providesTags: ["Inventory"],
     }),
 
     getSingleLoader: builder.query({
@@ -21,11 +23,22 @@ export const truckAddApi = createApi({
 
     myPersonalAdds: builder.query({
       query: () => ({
-        url: '/loader/currentuser',
+        url: "/loader/currentuser",
         headers: {
           Authorization: `Bearer ${token}`,
         },
       }),
+    }),
+
+    deleteLoader: builder.mutation({
+      query: (id) => ({
+        url: `/loader/${id}`,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ["Inventory"],
     }),
   }),
 });
@@ -34,4 +47,5 @@ export const {
   useGetAllLoadersQuery,
   useGetSingleLoaderQuery,
   useMyPersonalAddsQuery,
+  useDeleteLoaderMutation,
 } = truckAddApi;
