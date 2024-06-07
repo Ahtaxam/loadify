479dfaab618e19dfc9184c7357b197d2a1b3db17
ahtasham <ahtaxam03@gmail.com>
2024-06-07 23:56:13 +0500

    display chat users

---
+++ src/screens/chat/user.jsx
@@ -0,0 +1,31 @@
+import { Avatar } from "flowbite-react";
+import React, { useEffect } from "react";
+import useConversation from "../../zustand/userConversation";
+
+function User({ data }) {
+  const { _id, firstName, lastName } = data;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const handleSelectedUser = () => {
+    setSelectedConversation(data);
+
+  };
+
+
+
+  const isSelected = selectedConversation?._id === _id;
+  // console.log(isSelected);
+
+  return (
+    <div
+      className="shadow-lg flex p-2 items-center gap-4 border cursor-pointer "
+      onClick={handleSelectedUser}
+    >
+      <Avatar rounded></Avatar>
+      <p className="">
+        {firstName} {lastName}{" "}
+      </p>
+    </div>
+  );
+}
+
+export default User;
