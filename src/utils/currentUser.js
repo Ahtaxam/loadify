bec222d1627e9e1c57276da6258f80a29bde5a9b
ahtasham <ahtaxam03@gmail.com>
2024-06-07 23:55:13 +0500

    auth token for rtk

--- src/utils/currentUser.js
+++ src/utils/currentUser.js
@@ -1,4 +1,4 @@
-const KEY = 'user';
+const KEY = "user";
 export const storeCurrentUser = (data) => {
   sessionStorage.setItem(KEY, JSON.stringify(data));
 };
@@ -19,4 +19,12 @@ export const getToken = () => {
 
 export const logoutCurrentUser = () => {
   sessionStorage.removeItem(KEY);
+};
+
+export const authorizationHeader = (headers, storeState) => {
+  const result = JSON.parse(sessionStorage.getItem(KEY));
+  if (result) {
+    headers.set("Authorization", `Bearer ${result?.token}`);
+  }
+  return headers;
 };
