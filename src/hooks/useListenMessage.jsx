804293767f11113ff95941c6052c4ee19e377185
ahtasham <ahtaxam03@gmail.com>
2024-06-08 17:15:30 +0500

    made changes

--- src/hooks/useListenMessage.jsx
+++ src/hooks/useListenMessage.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import { useSocketContext } from "../context/socket";
 import useConversation from "../zustand/userConversation";
+import { getSocket } from "../context/socketEvent";
 
 function useListenMessage() {
-  const { socket } = useSocketContext();
+  // const {socket} = useSocketContext()
   const { messages, setMessages } = useConversation();
+
+  const socket = getSocket();
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
+      console.log(newMessage);
       setMessages([...messages, newMessage]);
     });
 
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -4,6 +4,7 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { truckAddApi } from "./api/truckadd";
 import { inventoryAddApi } from "./api/inventoryAdd";
 import { chatApi } from "./api/chatApi";
+import currentUserReducer from "./slice/currentUser";
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +12,7 @@ export const store = configureStore({
     [truckAddApi.reducerPath]: truckAddApi.reducer,
     [inventoryAddApi.reducerPath]: inventoryAddApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
+    currentUser: currentUserReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
