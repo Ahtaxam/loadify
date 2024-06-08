4ab997b3fd51c4f3dac434f35c95d6249f6dbf60
ahtasham <ahtaxam03@gmail.com>
2024-06-08 13:10:28 +0500

    fetch chatUser and display

--- src/screens/chat/chatUsers.jsx
+++ src/screens/chat/chatUsers.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGetUserChatsQuery } from "../../redux/api/chatApi";
 import User from "./user";
+import { useLocation } from "react-router-dom";
+import SpinnerComponent from "../../components/spinner";
+import useConversation from "../../zustand/userConversation";
+import { useSocketContext } from "../../context/socket";
 
 function ChatUsers() {
-  const { data } = useGetUserChatsQuery();
+  const { data, refetch, isLoading } = useGetUserChatsQuery();
+  const { selectedConversation } = useConversation();
+  const history = useLocation();
+
+  useEffect(() => {
+    refetch();
+  }, [history, refetch]);
+  const { onlineUsers } = useSocketContext();
+
+  const isOnline = onlineUsers.includes(selectedConversation?._id);
   return (
     <div className="flex flex-col gap-4">
-      {data?.map((obj,i) => (
-        <User data={obj} key={i} />
-      ))}
+      {isLoading ? (
+        <SpinnerComponent />
+      ) : (
+        data?.map((obj, i) => <User data={obj} key={i} isOnline={isOnline} />)
+      )}
     </div>
   );
 }
