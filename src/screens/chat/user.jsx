439b5119475d51d3492dc96ac0651d0c33054a2d
ahtasham <ahtaxam03@gmail.com>
2024-06-08 17:13:55 +0500

    refactor code

--- src/screens/chat/user.jsx
+++ src/screens/chat/user.jsx
@@ -2,24 +2,18 @@ import { Avatar } from "flowbite-react";
 import React, { useEffect } from "react";
 import useConversation from "../../zustand/userConversation";
 
-function User({ data,isonline }) {
+function User({ data,index,length }) {
   const { _id, firstName, lastName } = data;
   const { selectedConversation, setSelectedConversation } = useConversation();
   const handleSelectedUser = () => {
     setSelectedConversation(data);
-
   };
 
-
-
-  const isSelected = selectedConversation?._id === _id;
-  // console.log(isSelected);
   return (
     <div
-      className="shadow-lg flex p-2 items-center gap-4 border cursor-pointer "
+      className={`flex p-2 items-center gap-4  cursor-pointer ${index !== length-1 ? "border-b":""}`}
       onClick={handleSelectedUser}
     >
-      {isonline ? "online":""}
       <Avatar rounded></Avatar>
       <p className="">
         {firstName} {lastName}{" "}
