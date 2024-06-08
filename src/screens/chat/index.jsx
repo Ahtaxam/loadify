be9caa5daaccc391de6322028f536aa136d0f854
ahtasham <ahtaxam03@gmail.com>
2024-06-08 17:14:21 +0500

    show loading when post message

--- src/screens/chat/index.jsx
+++ src/screens/chat/index.jsx
@@ -12,6 +12,8 @@ import Messages from "./messages";
 import { useDispatch } from "react-redux";
 import NavbarComponent from "../../components/navbar";
 import FooterComponent from "../../components/footer";
+import { getCurrentUser } from "../../utils/currentUser";
+import { ImSpinner8 } from "react-icons/im";
 
 function Chat() {
   const {
@@ -23,6 +25,7 @@ function Chat() {
   const [userMessage, setUserMessage] = useState("");
   const [sendMessage, { isLoading }] = useSendMessageMutation();
   const dispatch = useDispatch();
+  const user = getCurrentUser();
 
   const handleSendMessage = async () => {
     if (userMessage === "") return;
@@ -43,57 +46,6 @@ function Chat() {
   }, []);
 
   return (
-    // <>
-    // <NavbarComponent/>
-    //   <div className="flex p-4 justify-center  bg-gray-100">
-    //     <div className=" w-[90%] bg-white rounded-lg shadow-lg overflow-hidden flex">
-    //       <div className="w-1/4 p-2 border-r border-gray-300">
-    //         <h2 className="text-xl font-semibold mb-4 text-center">Users</h2>
-    //         <ChatUsers />
-    //       </div>
-
-    //       {/* Chat Box */}
-    //       <div className="w-3/4 p-2 flex flex-col">
-    //         <div className="flex-1 overflow-y-auto  bg-gray-50 rounded-lg">
-    //           {selectedConversation ? (
-    //             <>
-    //               <div className="w-full h-12 bg-navy text-white flex items-center">
-    //                 <p className="ml-4">
-    //                   To: {selectedConversation.firstName}{" "}
-    //                   {selectedConversation.lastName}
-    //                 </p>
-    //               </div>
-    //               <div className="m-4">
-    //                 <Messages />
-    //               </div>
-    //             </>
-    //           ) : (
-    //             <p className="text-gray-500 text-center">
-    //               Select a user to start chatting
-    //             </p>
-    //           )}
-    //         </div>
-    //         {selectedConversation && (
-    //           <div className="mt-4 relative">
-    //             <input
-    //               type="text"
-    //               placeholder="Type your message..."
-    //               className="w-full p-2 border rounded-lg pr-10"
-    //               onChange={(e) => setUserMessage(e.target.value)}
-    //               value={userMessage}
-    //             />
-    //             <button
-    //               className="absolute right-2 top-2 text-blue-500"
-    //               onClick={handleSendMessage}
-    //             >
-    //               <IoSend size={24} />
-    //             </button>
-    //           </div>
-    //         )}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
     <>
       <NavbarComponent />
       <div className="flex p-4 justify-center bg-gray-100 h-screen ">
@@ -128,14 +80,21 @@ function Chat() {
                     className="absolute right-2 top-2 text-blue-500"
                     onClick={handleSendMessage}
                   >
-                    <IoSend size={24} />
+                    {isLoading ? (
+                      <ImSpinner8 size={24} className="animate-spin" />
+                    ) : (
+                      <IoSend size={24} />
+                    )}
                   </button>
                 </div>
               </>
             ) : (
-              <p className="text-gray-500 text-center flex-1">
-                Select a user to start chatting
-              </p>
+              <div className="text-gray-500 text-center flex items-center justify-center flex-col flex-1">
+                <p>
+                  Hello {user?.firstName} {user?.lastName}{" "}
+                </p>
+                <p>Select a user to start chatting</p>
+              </div>
             )}
           </div>
         </div>
