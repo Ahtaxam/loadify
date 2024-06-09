3af9302d884ffce058c6be0de24fc1d3ce318d96
ahtasham <ahtaxam03@gmail.com>
2024-06-09 13:02:25 +0500

    add booking end point

---
+++ src/redux/api/booking.jsx
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { authorizationHeader } from "../../utils/currentUser";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const bookingApi = createApi({
+  reducerPath: "bookingApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      return authorizationHeader(headers, getState);
+    },
+  }),
+  tagTypes: ["Booking"],
+  endpoints: (builder) => ({
+    shippInventory: builder.mutation({
+      query: (data) => ({
+        url: "/booking/shipped",
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Booking"],
+    }),
+    getAllActiveAdds: builder.query({
+      query: () => ({
+        url: "/booking/active",
+      }),
+      providesTags: ["Booking"],
+    }),
+  }),
+});
+
+export const { useShippInventoryMutation, useGetAllActiveAddsQuery } =
+  bookingApi;
