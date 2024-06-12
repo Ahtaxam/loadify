3c34fb916a39e928aebe2d9496ddde6d250fca0a
Ahtaxam <ahtaxam03@gmail.com>
2024-06-12 12:15:31 +0500

    made changes in login component

--- src/screens/auth/Login/index.jsx
+++ src/screens/auth/Login/index.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import FormikController from "../../../components/formik/formikController";
-import Button from "../../../components/button";
-import { Link, useNavigate } from "react-router-dom";
-import { PATH } from "../../../utils/path";
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormikController from '../../../components/formik/formikController';
+import Button from '../../../components/button';
+import { Link, useNavigate } from 'react-router-dom';
+import { PATH } from '../../../utils/path';
 import { useLoginUserMutation } from '../../../redux/api/user';
 import { storeCurrentUser } from '../../../utils/currentUser';
 import { toast } from 'react-toastify';
-import { useDispatch } from "react-redux";
-import { storeUser } from "../../../redux/slice/currentUser";
-import { connectToSocket } from "../../../context/socketEvent";
+import { useDispatch } from 'react-redux';
+import { storeUser } from '../../../redux/slice/currentUser';
+import { connectToSocket } from '../../../context/socketEvent';
 // import { connectToSocket } from "../../../context/socket";
 
-function Login() {
-  const [loginUser, {isLoading} ] = useLoginUserMutation();
+function Login({ className, closeModal, notNavigation }) {
+  const [loginUser, { isLoading }] = useLoginUserMutation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const initialValues = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   };
 
   const validationSchema = Yup.object({
-    email: Yup.string().required("Email is required"),
-    password: Yup.string().required("Password is required").min(8),
+    email: Yup.string().required('Email is required'),
+    password: Yup.string().required('Password is required').min(8),
   });
 
   const handleSubmit = async (values) => {
@@ -32,9 +32,14 @@ function Login() {
       const { message, data, token } = await loginUser(values).unwrap();
       toast.success(message);
       storeCurrentUser({ ...data, token });
-      dispatch(storeUser(data))
-      connectToSocket(data?._id)
-      navigate(PATH.HOME)
+      dispatch(storeUser(data));
+      connectToSocket(data?._id);
+      if (notNavigation) {
+        closeModal();
+        return;
+      } else {
+        navigate(PATH.HOME);
+      }
     } catch (error) {
       toast.error(error?.data?.message);
       console.log(error);
@@ -42,9 +47,15 @@ function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen p-2">
-      <div className="bg-[#f5f5f5] w-full max-w-lg p-8 rounded-2xl">
-        <p className="text-center font-extrabold text-xl">Login</p>
+    <div
+      className={`${
+        className
+          ? className
+          : 'flex justify-center items-center min-h-screen p-2'
+      } `}
+    >
+      <div className='bg-[#f5f5f5] w-full max-w-lg p-8 rounded-2xl'>
+        <p className='text-center font-extrabold text-xl'>Login</p>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -53,25 +64,25 @@ function Login() {
           {(formik) => (
             <Form>
               <FormikController
-                control="input"
-                type="email"
-                label="Email"
-                name="email"
+                control='input'
+                type='email'
+                label='Email'
+                name='email'
               />
 
               <FormikController
-                control="input"
-                type="password"
-                label="Password"
-                name="password"
+                control='input'
+                type='password'
+                label='Password'
+                name='password'
               />
 
-              <Button type="submit" className="w-full">
-                {isLoading ? "Logging...":"Login"}
+              <Button type='submit' className='w-full'>
+                {isLoading ? 'Logging...' : 'Login'}
               </Button>
-              <p className="text-sm text-right">
-                Don't have an account?{" "}
-                <Link to={PATH.SIGNUP} className="underline">
+              <p className='text-sm text-right'>
+                Don't have an account?{' '}
+                <Link to={PATH.SIGNUP} className='underline'>
                   Create one
                 </Link>
               </p>
