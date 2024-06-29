d24006d4b50f477e0a48c77fee452cd5a9aeabcc
ahtasham <ahtaxam03@gmail.com>
2024-06-29 13:40:17 +0500

    implement contactus form

--- src/screens/contactUs/index.jsx
+++ src/screens/contactUs/index.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import NavbarComponent from "../../components/navbar";
 import FooterComponent from "../../components/footer";
+import { Form, Formik } from "formik";
+import FormikController from "../../components/formik/formikController";
+import Button from "../../components/button";
+import { useContactUsMutation } from "../../redux/api/contactUs";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
 
 function ContactUs() {
+  const [contactUs, { isLoading }] = useContactUsMutation();
+  const initialValue = {
+    name: "",
+    message: "",
+    email: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Email is required"),
+    name: Yup.string().required("Name is required"),
+    message: Yup.string().required("Message is required"),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      const { message } = await contactUs(values).unwrap();
+      toast.success(message);
+      resetForm();
+    } catch (error) {
+      toast.error(error?.data?.message || "SERVER ERROR");
+      console.log(error);
+    }
+  };
   return (
     <>
       <NavbarComponent />
@@ -17,72 +45,43 @@ function ContactUs() {
         </div>
       </div>
 
-      {/* <div className="max-w-[500px] mt-[50px] ml-[100px] py-8">
-        <p className="text-4xl mb-4 text-gray-400">Contact US</p>
-        <p>
-          For inquiries regarding our services, support, or any other questions,
-          please use the following channels to reach us. For urgent matters,
-          kindly contact our hotline. If you're reaching out outside of our
-          operating hours, please leave a message and we will return your call
-          at the earliest possible opportunity.
-        </p>
-      </div> */}
-
       <div className="max-w-3xl mx-auto mt-12 p-6 bg-white shadow-md rounded-lg">
         <h2 className="text-3xl font-bold mb-6 text-gray-800">Get in Touch</h2>
-        <form className="space-y-4">
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your Name"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Your Email"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="message"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows={4}
-              placeholder="Your Message"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <button
-            type="submit"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Submit
-          </button>
-        </form>
+        <Formik
+          initialValues={initialValue}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {(formik) => (
+            <Form>
+              <FormikController
+                control="input"
+                type="text"
+                label="Name"
+                name="name"
+                placeholder="Your Name"
+              />
+              <FormikController
+                control="input"
+                type="email"
+                label="Email"
+                name="email"
+                placeholder="Your Email"
+              />
+
+              <FormikController
+                control="textarea"
+                type="text"
+                label="Message"
+                name="message"
+                placeholder="Your Message"
+              />
+              <Button type="submit" className="w-full">
+                {isLoading ? "Logging..." : "Contact"}
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </div>
 
       <div className="max-w-3xl mt-12 p-6 bg-white  rounded-lg">
@@ -104,7 +103,7 @@ function ContactUs() {
           Address: 123 Main Street, City, Country
         </p>
       </div>
-      <FooterComponent/>
+      <FooterComponent />
     </>
   );
 }
