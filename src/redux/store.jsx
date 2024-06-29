118194facc5915c33c8dcbdfa0fdb07a7060c979
ahtasham <ahtaxam03@gmail.com>
2024-06-29 13:39:13 +0500

    implement redux api endpoints

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -6,6 +6,8 @@ import { inventoryAddApi } from "./api/inventoryAdd";
 import { chatApi } from "./api/chatApi";
 import currentUserReducer from "./slice/currentUser";
 import { bookingApi } from "./api/booking";
+import { passwordApi } from "./api/password";
+import { contactUsApi } from "./api/contactUs";
 
 export const store = configureStore({
   reducer: {
@@ -14,6 +16,8 @@ export const store = configureStore({
     [inventoryAddApi.reducerPath]: inventoryAddApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
     [bookingApi.reducerPath]: bookingApi.reducer,
+    [passwordApi.reducerPath]: passwordApi.reducer,
+    [contactUsApi.reducerPath]: contactUsApi.reducer,
     currentUser: currentUserReducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -22,7 +26,9 @@ export const store = configureStore({
       truckAddApi.middleware,
       inventoryAddApi.middleware,
       bookingApi.middleware,
-      chatApi.middleware
+      chatApi.middleware,
+      passwordApi.middleware,
+      contactUsApi.middleware
     ),
 });
 
