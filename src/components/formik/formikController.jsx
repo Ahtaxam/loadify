ad2fa45870b24df6c574c4000f363cc2439c13b2
ahtasham <ahtaxam03@gmail.com>
2024-06-29 13:38:32 +0500

    add formik textarea

--- src/components/formik/formikController.jsx
+++ src/components/formik/formikController.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Input from "./input";
 import Select from "./select";
+import TextArea from "./textarea";
 
 function FormikController(props) {
   const { control, ...rest } = props;
@@ -9,7 +10,9 @@ function FormikController(props) {
     case "input":
       return <Input {...rest} />;
     case "select":
-      return <Select {...rest} />
+      return <Select {...rest} />;
+    case "textarea":
+      return <TextArea {...rest} />;
 
     default:
       return;
