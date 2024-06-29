a5f61e1e3a26824665c526254e532bd76211f4a5
ahtasham <ahtaxam03@gmail.com>
2024-06-30 01:10:21 +0500

    change chat scroll bahaviour

--- src/screens/chat/messages.jsx
+++ src/screens/chat/messages.jsx
@@ -6,20 +6,30 @@ import useListenMessage from "../../hooks/useListenMessage";
 
 function Messages() {
   const { messages, loading } = useGetMesseges();
-  const scrollRef = useRef();
+  const scrollRef = useRef(null);
   useListenMessage();
+
   useEffect(() => {
     setTimeout(() => {
-      scrollRef.current?.scrollIntoView({ behaviour: "smooth" });
+      if (scrollRef.current) {
+        scrollRef.current.scrollTo({
+          top: scrollRef.current.scrollHeight,
+          behavior: "smooth",
+        });
+      }
     }, 100);
   }, [messages]);
+
   return (
-    <div className="p-4 bg-gray-50 h-full rounded-lg overflow-y-auto">
+    <div
+      className="p-4 bg-gray-50 h-full rounded-lg overflow-y-auto"
+      ref={scrollRef}
+    >
       {loading ? (
         <MessagesSkeleton />
       ) : (
         messages?.map((message, i) => (
-          <div key={i} ref={scrollRef}>
+          <div key={i}>
             <Message obj={message} />
           </div>
         ))
